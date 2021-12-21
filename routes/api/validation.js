c29ec7662d7b1c30077f904aa9ae151e548f81ce
Sta11k <Alexandiel1984@gmail.com>
2021-12-21 16:55:28 +0200

    start

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,52 +1,47 @@
-import Joi from 'joi'
+import Joi from "joi";
 
 const newSchema = Joi.object({
-    name: Joi.string().min(2).max(33).required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required()
-})
+  name: Joi.string().min(2).max(33).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
 const updateSchema = Joi.object({
-    name: Joi.string().optional(),
-    email: Joi.string().email().optional(),
-    phone: Joi.string().optional(),
-}).or('name', 'email', 'phone')
+  name: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string().optional(),
+}).or("name", "email", "phone");
 
-const idSchema = Joi.object({id:Joi.string().required()})
+const idSchema = Joi.object({ id: Joi.string().required() });
 
+export const validateCreation = async (req, res, next) => {
+  try {
+    const value = await newSchema.validateAsync(req.body);
+  } catch (err) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+  next();
+};
 
-export const validateCreation = async (req, res, next)=>{ 
-
-    try {
-        const value = await newSchema.validateAsync(req.body)
-    } catch (err) {
-        return res.status(400).json({"message": "missing fields"})
+export const validateUpdate = async (req, res, next) => {
+  try {
+    const value = await updateSchema.validateAsync(req.body);
+  } catch (err) {
+    const [{ type }] = err.detalis;
+    if (type === "object.unknown") {
+      return res.status(400).json({ message: err.message });
     }
-    next()
-}
+    return res.status(400).json({ message: "missing fields" });
+  }
+  next();
+};
 
-
-export const validateUpdate = async (req, res, next)=>{ 
-
-    try {
-        const value = await updateSchema.validateAsync(req.body)
-    } catch (err) {
-        const [{ type }] = err.detalis
-        if (type === 'object.unknown') {
-              return res.status(400).json({"message": err.message})
-         }
-        return res.status(400).json({"message": "missing fields"})
-    }
-    next()
-}
-
-
-export const validateId = async (req, res, next)=>{ 
-
-    try {
-        const value = await idSchema.validateAsync(req.params)
-    } catch (err) {
-        return res.status(400).json({"message": err.message})
-    }
-    next()
-}
+export const validateId = async (req, res, next) => {
+  try {
+    const value = await idSchema.validateAsync(req.params);
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+  next();
+};
+//server
