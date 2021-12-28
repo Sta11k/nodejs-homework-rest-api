744e880ed88a58b256f2b40b7d85100feb71f008
Sta11k <Alexandiel1984@gmail.com>
2021-12-28 10:13:38 +0200

    creat-constans

--- repository/contacts.js
+++ repository/contacts.js
@@ -1,31 +1,37 @@
 import Contact from "../model/contact";
 
-
-const listContacts = async ({ sortBy, sortByDesc, filter, limit = 10, skip = 0, }) => {
-  let sortCriteria=null
-  const total=await Contact.find().countDocuments()
+const listContacts = async ({
+  sortBy,
+  sortByDesc,
+  filter,
+  limit = 10,
+  skip = 0,
+}) => {
+  let sortCriteria = null;
+  const total = await Contact.find().countDocuments();
   let result = Contact.find();
-  if (sortBy) { 
-    sortCriteria = {[`${sortBy}`]:1}
+  if (sortBy) {
+    sortCriteria = { [`${sortBy}`]: 1 };
   }
-   if (sortByDesc) { 
-    sortCriteria = {[`${sortByDesc}`]:-1}
+  if (sortByDesc) {
+    sortCriteria = { [`${sortByDesc}`]: -1 };
   }
-  if (filter) { 
-    result = result.select(filter.split('|').join(' '))
+  if (filter) {
+    result = result.select(filter.split("|").join(" "));
   }
-  result=await result.skip(Number(skip)).limit(Number(limit)).sort(sortCriteria)
-  return {total, contacts:result};
+  result = await result
+    .skip(Number(skip))
+    .limit(Number(limit))
+    .sort(sortCriteria);
+  return { total, contacts: result };
 };
 
 const getContactById = async (contactId) => {
- 
   const result = await Contact.findById(contactId);
   return result;
 };
 
 const removeContact = async (contactId) => {
-
   const result = await Contact.findByIdAndRemove(contactId);
   return result;
 };
@@ -36,9 +42,8 @@ const addContact = async (body) => {
 };
 
 const updateContact = async (contactId, body) => {
-
-  const  result  = await Contact.findByIdAndUpdate(
-   contactId,
+  const result = await Contact.findByIdAndUpdate(
+    contactId,
     { ...body },
     { new: true }
   );
--- routes/contacts/validation.js
+++ routes/contacts/validation.js
@@ -1,15 +1,15 @@
 import Joi from "joi";
-import mongoose from 'mongoose';
-import { MAX_AGE, MIN_AGE, SKIP_ZERO } from "../../lib/constants";
-const { Types  } = mongoose;
-
+import mongoose from "mongoose";
+import { HttpCode, MAX_AGE, MIN_AGE, SKIP_ZERO } from "../../lib/constants";
+import { HttpMessage } from "../../lib/message";
+const { Types } = mongoose;
 
 const newSchema = Joi.object({
   name: Joi.string().min(2).max(33).required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
   age: Joi.number().integer().min(MIN_AGE).max(MAX_AGE).optional(),
-  favorite:Joi.bool().optional(),
+  favorite: Joi.bool().optional(),
 });
 
 const updateSchema = Joi.object({
@@ -17,77 +17,83 @@ const updateSchema = Joi.object({
   email: Joi.string().email().optional(),
   phone: Joi.string().optional(),
   age: Joi.number().integer().min(MIN_AGE).max(MAX_AGE).optional(),
-  favorite:Joi.bool().optional()
+  favorite: Joi.bool().optional(),
 }).or("name", "email", "phone", "age");
 
 const updateFavoriteSchema = Joi.object({
-  favorite:Joi.bool().required(),
-})
-const regularLimit = '\\d+';
+  favorite: Joi.bool().required(),
+});
+const regularLimit = "\\d+";
 const querySchema = Joi.object({
- 
   // eslint-disable-next-line prefer-regex-literals
   limit: Joi.string().pattern(new RegExp(regularLimit)).optional(),
   skip: Joi.number().min(SKIP_ZERO).optional(),
-  sortBy: Joi.string().valid('name', 'age', 'email', 'phone', 'favorite'),
-  sortByDesc: Joi.string().valid('name', 'age', 'email', 'phone', 'favorite'),  
+  sortBy: Joi.string().valid("name", "age", "email", "phone", "favorite"),
+  sortByDesc: Joi.string().valid("name", "age", "email", "phone", "favorite"),
   // eslint-disable-next-line prefer-regex-literals
-  filter:Joi.string().pattern(new RegExp('(name|email|age)\\|?(name|email|age)+')).optional(),
-})
-
+  filter: Joi.string()
+    .pattern(new RegExp("(name|email|age)\\|?(name|email|age)+"))
+    .optional(),
+});
 
 export const validateCreation = async (req, res, next) => {
   try {
     await newSchema.validateAsync(req.body);
   } catch (err) {
-    return res.status(400).json({ message: "missing fields" });
+    return res
+      .status(HttpCode.BAD_REQUEST)
+      .json({ message: HttpMessage.MISSING_FIELDS });
   }
   next();
 };
 
 export const validateUpdate = async (req, res, next) => {
   try {
-  await updateSchema.validateAsync(req.body);
+    await updateSchema.validateAsync(req.body);
   } catch (err) {
     const [{ type }] = err.details;
     if (type === "object.missing") {
-      return res.status(400).json({ message: 'missing fields' });
+      return res
+        .status(HttpCode.BAD_REQUEST)
+        .json({ message: HttpMessage.MISSING_FIELDS });
     }
-    return res.status(400).json({ message: err.message });
+    return res.status(HttpCode.BAD_REQUEST).json({ message: err.message });
   }
   next();
 };
 
 export const validateUpdateFavorite = async (req, res, next) => {
   try {
-  await updateFavoriteSchema.validateAsync(req.body);
+    await updateFavoriteSchema.validateAsync(req.body);
   } catch (err) {
     const [{ type }] = err.details;
-    if (type === 'object.missing') {
-      return res.status(400).json({ message: 'missing field favorite' });
+    if (type === "object.missing") {
+      return res
+        .status(HttpCode.BAD_REQUEST)
+        .json({ message: HttpMessage.MISSING_FIELDS_FAVORITE });
     }
-    return res.status(400).json({ message: err.message });
-  }
-  next();
-}
-
-
-export const validateId = async (req, res, next) => {
-  if (!Types.ObjectId.isValid(req.params.is)) { 
-    return res.status(400).json({ message:'Invalid ObjectId'})
+    return res.status(HttpCode.BAD_REQUEST).json({ message: err.message });
   }
   next();
 };
 
+export const validateId = async (req, res, next) => {
+  if (!Types.ObjectId.isValid(req.params.is)) {
+    return res
+      .status(HttpCode.BAD_REQUEST)
+      .json({ message: HttpMessage.INVALID_OBJECTID });
+  }
+  next();
+};
 
 export const validateQuery = async (req, res, next) => {
   try {
     await querySchema.validateAsync(req.query);
   } catch (err) {
-    return res.status(400)
-      .json({ message: "missing fields" });
+    return res
+      .status(HttpCode.BAD_REQUEST)
+      .json({ message: HttpMessage.MISSING_FIELDS });
     //  .json({ message: ` Field ${err.message.replace(/"/g, '')}` });
-
   }
   next();
 };
