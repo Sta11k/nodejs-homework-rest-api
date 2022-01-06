b55527c1a9fb821db20a43fedcec1f35c5acc5e3
Sta11k <Alexandiel1984@gmail.com>
2022-01-06 15:43:54 +0200

    здача

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { HttpCode } from "./lib/constants";
 import { HttpMessage } from "./lib/message";
 import contactsRouter from "./routes/contacts/index";
 import authRouter from "./routes/auth/index";
-
+import usersRouter from "./routes/users/index";
 
 const app = express();
 
@@ -14,19 +14,31 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  app.set("lang", req.acceptsLanguages(["en", "ua"]));
+  next();
+});
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
-   res
-        .status(HttpCode.NOT_FOUND)
-        .json({ status:HttpMessage.NOT_FOUND, code:HttpCode.NOT_FOUND, message: HttpMessage.NOT_FOUND });
+  res.status(HttpCode.NOT_FOUND).json({
+    status: HttpMessage.NOT_FOUND,
+    code: HttpCode.NOT_FOUND,
+    message: HttpMessage.NOT_FOUND,
+  });
 });
 
 app.use((err, req, res, next) => {
-  res.status(HttpCode.INTERNAL_SERVER_ERROR).json({status:HttpCode.INTERNAL_SERVER_ERROR, code:HttpMessage.INTERNAL_SERVER_ERROR, message: err.message });
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+    status: HttpCode.INTERNAL_SERVER_ERROR,
+    code: HttpMessage.INTERNAL_SERVER_ERROR,
+    message: err.message,
+  });
 });
 
 export default app;
